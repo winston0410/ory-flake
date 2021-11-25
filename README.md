16342aad934ca4b56a867b64a5d6295ab599b569
kghugo <hugosum.dev@protonmail.com>
2021-11-25 11:28:14 +0000

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+docker run -v /home/hugosum/ory-kratos-flake/config:/etc/kratos oryd/kratos:latest serve --config /etc/kratos/kratos.yaml
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      nixosModule = { pkgs, config, ... }: {
+        virtualisation.oci-containers.containers."ory-kratos" = let
+        in {
+          image = "oryd/kratos:latest";
+          volumes = [
+
+          ];
+        };
+      };
+    in {
+      inherit nixosModule;
+      nixosModules = { default = nixosModule; };
+      defaultPackage.x86_64-linux = let
+        # kratosConfig = pkgs.writeText "kratos.yml" (builtins.readFile ./kratos.yml);
+        kratosConfig = pkgs.stdenv.mkDerivation {
+            name = "krato-config";
+            src = ./kratos.yml;
+        };
+      in pkgs.dockerTools.buildImage {
+        name = "oryd-kratos";
+        tag = "latest";
+        fromImage = pkgs.dockerTools.pullImage {
+          imageName = "oryd/kratos";
+          imageDigest = "sha256:11a8241b380830bf2254a8563a5f84fb4bbee9f40e869ff3a17bba3b4ac835a4";
+          sha256 = pkgs.lib.fakeSha256;
+        };
+
+        contents = [ kratosConfig ];
+
+        config = { 
+            Cmd = ["ls -a" "echo hello" ]; 
+        };
+      };
+    };
+}
